f5f5703f1794d2bedd2bde2714f1ae54bf7abeff
pool_1541 <pool_1541@hotmail.com>
2024-08-08 01:09:41 -0500

    chore(page): fix page component name

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import { SignIn } from "@/components/sign-in";
 
-export default function page() {
+export default function Page() {
   return (
     <div className="bg-slate-900 h-full">
       <SignIn />
