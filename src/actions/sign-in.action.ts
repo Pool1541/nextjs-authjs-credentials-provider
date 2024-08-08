02845989f20ec3f0e4f6cdb97b97f5b3efcb87a3
Pool Llerena <pool_1541@hotmail.com>
2024-08-08 11:37:09 -0500

    chore: Refactor sign-in and sign-up actions to handle CredentialsSignin errors

--- src/actions/sign-in.action.ts
+++ src/actions/sign-in.action.ts
@@ -2,37 +2,41 @@
 
 import { signIn } from '@/auth';
 import { signInSchema } from '@/utils/zod';
+import { CredentialsSignin } from 'next-auth';
 import { isRedirectError } from 'next/dist/client/components/redirect';
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 
-export async function authenticate(state: any, formData: FormData): Promise<{ success: boolean, errors: any }> {
+export async function authenticate(
+	state: any,
+	formData: FormData,
+): Promise<{ success: boolean; errors: any }> {
+	const validationResult = signInSchema.safeParse({
+		email: formData.get('email'),
+		password: formData.get('password'),
+	});
 
-  const validationResult = signInSchema.safeParse({
-    email: formData.get('email'),
-    password: formData.get('password')
-  });
+	if (!validationResult.success) {
+		return {
+			success: false,
+			errors: validationResult.error?.flatten().fieldErrors,
+		};
+	}
 
-  if (!validationResult.success) {
-    return {
-      success: false,
-      errors: validationResult.error?.flatten().fieldErrors,
-    }
-  }
+	try {
+		await signIn('credentials', validationResult.data);
+		return redirect('/dashboard');
+	} catch (error) {
+		if (isRedirectError(error)) {
+			return redirect('/dashboard');
+		}
+		if (error instanceof CredentialsSignin) {
+			// console.log(error);
+			const errorMessage = error.message.split('.')[0];
+			return { success: false, errors: errorMessage };
+		}
 
-  try {
-    await signIn('credentials', validationResult.data);
-    return redirect('/dashboard');
-  } catch (error) {
-    if (isRedirectError(error)) {
-      return redirect('/dashboard');
-    }
-    if (error instanceof Error) {
-      console.log(error);
-      return { success: false, errors: error.cause?.err.message };
-    }
-
-    return { success: false, errors: 'unknown error' };
-  }
+		return { success: false, errors: 'unknown error' };
+	}
 }
 
 // export async function authenticate(state: any, formData: FormData): Promise<{data: any, errors: any}> {
--- src/actions/sign-up.action.ts
+++ src/actions/sign-up.action.ts
@@ -5,43 +5,44 @@ import { signUpSchema } from '@/utils/zod';
 import { isRedirectError } from 'next/dist/client/components/redirect';
 import { redirect } from 'next/navigation';
 import userRepository from '@/repository/user.repository';
-
+import { CredentialsSignin } from 'next-auth';
 
 export async function createUser(
-  state: any,
-  formData: FormData
+	state: any,
+	formData: FormData,
 ): Promise<{ success: boolean; errors: any }> {
-  const validationResult = signUpSchema.safeParse({
-    name: formData.get('name'),
-    email: formData.get('email'),
-    password: formData.get('password'),
-  });
+	const validationResult = signUpSchema.safeParse({
+		name: formData.get('name'),
+		email: formData.get('email'),
+		password: formData.get('password'),
+	});
 
-  if (!validationResult.success) {
-    return {
-      success: false,
-      errors: validationResult.error?.flatten().fieldErrors,
-    };
-  }
+	if (!validationResult.success) {
+		return {
+			success: false,
+			errors: validationResult.error?.flatten().fieldErrors,
+		};
+	}
 
-  await userRepository.saveUser({
-    email: validationResult.data.email,
-    name: validationResult.data.name,
-    password: validationResult.data.password
-  });
+	await userRepository.saveUser({
+		email: validationResult.data.email,
+		name: validationResult.data.name,
+		password: validationResult.data.password,
+	});
 
-  try {
-    await signIn('credentials', validationResult.data);
-    return redirect('/dashboard');
-  } catch (error) {
-    if (isRedirectError(error)) {
-      return redirect('/dashboard');
-    }
-    if (error instanceof Error) {
-      console.log(error);
-      return { success: false, errors: error.cause?.err.message };
-    }
+	try {
+		await signIn('credentials', validationResult.data);
+		return redirect('/dashboard');
+	} catch (error) {
+		if (isRedirectError(error)) {
+			return redirect('/dashboard');
+		}
+		if (error instanceof CredentialsSignin) {
+			// console.log(error);
+			const errorMessage = error.message.split('.')[0];
+			return { success: false, errors: errorMessage };
+		}
 
-    return { success: false, errors: 'unknown error' };
-  }
+		return { success: false, errors: 'unknown error' };
+	}
 }
