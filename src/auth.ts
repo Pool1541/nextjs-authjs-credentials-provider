3a8ec93f4cb4f0a3696a945b92e1eee7673c4fea
Pool Llerena <pool_1541@hotmail.com>
2024-08-08 10:55:22 -0500

    chore(auth): update authjs config

--- src/auth.ts
+++ src/auth.ts
@@ -1,39 +1,38 @@
-import NextAuth, { CredentialsSignin, Session } from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import { signInSchema } from "./utils/zod";
-import { NextRequest } from "next/server";
-import { verify } from 'jsonwebtoken'
-import userRepository from '@/repository/user.repository'
+import NextAuth, {CredentialsSignin, Session} from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import {signInSchema} from "./utils/zod";
+import {NextRequest} from "next/server";
+import {verify} from "jsonwebtoken";
+import userRepository from "@/repository/user.repository";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const {handlers, signIn, signOut, auth} = NextAuth({
   providers: [
     Credentials({
       credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
+        email: {label: "Email", type: "text"},
+        password: {label: "Password", type: "password"},
       },
       authorize: async (credentials) => {
-        const { email, password } = await signInSchema.parseAsync(credentials)
+        const {email, password} = await signInSchema.parseAsync(credentials);
         const user = await userRepository.getUserByEmail(email);
 
         if (!user) {
           throw new Error("Invalid email or password");
         }
+
         if (user.password !== password) {
           throw new CredentialsSignin("Invalid email or password");
         }
+
+        return {id: user.id, name: user.name, email: user.email};
         
-        return { id: user.id, name: user.name, email: user.email };
 
 
-        // // Lógica de autenticación personalizada
-        // if (email === "pool_1541@hotmail.com" && password === "password123") {
-        //   // Retornar el objeto de usuario si las credenciales son válidas
-        //   return { id: "1", name: "John Doe", email: "pool_1541@hotmail.com" };
-        // } else {
-        //   // Lanzar un error si las credenciales no son válidas
-        //   throw new Error("Invalid email or password");
-        // }
+
+
+
+
+        
       },
     }),
   ],
@@ -43,7 +42,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   secret: "my-secret",
   callbacks: {
-    authorized: async ({request, auth}: { request: NextRequest, auth: Session | null}) => {
+    authorized: async ({request, auth}: {request: NextRequest; auth: Session | null}) => {
       const protectedRoutes = ["/dashboard"];
       const url = request.nextUrl.pathname;
       const isProtectedRoute = protectedRoutes.includes(url);
@@ -51,8 +50,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       if (!isProtectedRoute) return true;
 
       if (auth) {
-        const { user, expires } = auth;
-        console.log('authorized', { user, expires});
+        const {user, expires} = auth;
+        console.log("authorized", {user, expires});
         // TODO: Validar si la sesión está expirada
         if (!user) {
           return false;
@@ -63,20 +62,20 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         return false;
       }
     },
-    signIn: async ({ user, account }) => {
+    signIn: async ({user, account}) => {
       // it is called before then "authorized" callback
-      console.log('signIn-callback', user, account);
-      return true
-    }
+      console.log("signIn-callback", user, account);
+      return true;
+    },
   },
   pages: {
-    signIn: '/auth/sign-in',
-    newUser: '/auth/sign-up',
+    signIn: "/auth/sign-in",
+    newUser: "/auth/sign-up",
   },
   events: {
-    createUser: async ({ user }) => {
+    createUser: async ({user}) => {
       console.log("User created", user);
-    }
+    },
   },
 });
 
@@ -87,12 +86,12 @@ function validateAuthToken(authToken: any) {
   // You can use libraries like jsonwebtoken to help with this
 
   // Here's an example implementation using jsonwebtoken library
-  const secret = 'my-secret';
+  const secret = "my-secret";
 
   try {
     // Verify the authToken using the secret key
     const decoded = verify(authToken, secret);
-    console.log(decoded)
+    console.log(decoded);
     // If the verification is successful, return true
     return true;
   } catch (error) {
@@ -100,5 +99,3 @@ function validateAuthToken(authToken: any) {
     return false;
   }
 }
-
-
