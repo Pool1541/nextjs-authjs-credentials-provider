9ff6465037a861cb2cefedefd3f63b6ce18476b9
pool_1541 <pool_1541@hotmail.com>
2024-08-10 20:15:08 -0500

    chore: Update import path for SendEmailVerificationButton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next';
 import Link from 'next/link';
-import SendEmailVerificationButton from '../components/send-email-verification-button';
+import SendEmailVerificationButton from '@/components/send-email-verification-button';
 
 export const metadata: Metadata = {
 	title: 'Home | Acme',
@@ -12,8 +12,10 @@ export default function Home() {
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div>
 				<h1>Home</h1>
-				<Link href={'/dashboard'} className='text-slate-100 underline underline-offset-4'>Dashboard</Link>
-				<div className='size-14'></div>
+				<Link href={'/dashboard'} className="text-slate-100 underline underline-offset-4">
+					Dashboard
+				</Link>
+				<div className="size-14"></div>
 				<SendEmailVerificationButton />
 			</div>
 		</main>
